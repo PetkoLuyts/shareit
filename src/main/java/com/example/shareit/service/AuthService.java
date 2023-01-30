ec6f5153191b61ab32de44b4348cf05e88055d0b
petko.lyutskanov <simona2000petko@abv.bg>
2023-01-31 00:28:32 +0200

    Implement generate verification token method in auth service

--- src/main/java/com/example/shareit/service/AuthService.java
+++ src/main/java/com/example/shareit/service/AuthService.java
@@ -2,21 +2,24 @@ package com.example.shareit.service;
 
 import com.example.shareit.dto.RegisterRequest;
 import com.example.shareit.model.User;
+import com.example.shareit.model.VerificationToken;
 import com.example.shareit.repository.UserRepository;
+import com.example.shareit.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
-
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -28,5 +31,18 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 }
