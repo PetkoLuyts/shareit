b77d3f2ca0991b076cb20ef68fe189e6b264dac8
petko.lyutskanov <simona2000petko@abv.bg>
2023-01-31 00:21:05 +0200

    Implement save new user method in auth service

--- src/main/java/com/example/shareit/service/AuthService.java
+++ src/main/java/com/example/shareit/service/AuthService.java
@@ -1,12 +1,32 @@
 package com.example.shareit.service;
 
 import com.example.shareit.dto.RegisterRequest;
+import com.example.shareit.model.User;
+import com.example.shareit.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.Instant;
+
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    public void signup(RegisterRequest registerRequest) {
+    private final PasswordEncoder passwordEncoder;
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
     }
 }
