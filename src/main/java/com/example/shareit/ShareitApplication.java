ff505b08a63fbdda7f1dee393fe001420f81e1d5
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-07 09:46:41 +0200

    Enable async processing

--- src/main/java/com/example/shareit/ShareitApplication.java
+++ src/main/java/com/example/shareit/ShareitApplication.java
@@ -2,8 +2,10 @@ package com.example.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ShareitApplication {
 
     public static void main(String[] args) {
