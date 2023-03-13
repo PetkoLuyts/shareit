45e8c38f8353b71458c98de187b456faff50a5a7
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-13 09:13:08 +0200

    Implement Get API to retrieve all subreddits

--- src/main/java/com/example/shareit/service/SubredditService.java
+++ src/main/java/com/example/shareit/service/SubredditService.java
@@ -6,6 +6,11 @@ import com.example.shareit.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
@@ -14,11 +19,27 @@ public class SubredditService {
 
     private final SubredditRepository subredditRepository;
 
+    @Transactional
     public SubredditDto save(SubredditDto subredditDto) {
         Subreddit save = subredditRepository.save(mapSubredditDto(subredditDto));
         subredditDto.setId(save.getId());
 
         return subredditDto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<SubredditDto> getAll() {
+        return subredditRepository.findAll()
+                .stream()
+                .map(this::mapToDto)
+                .collect(toList());
+    }
+
+    private SubredditDto mapToDto(Subreddit subreddit) {
+        return SubredditDto.builder().name(subreddit.getName())
+                .id(subreddit.getId())
+                .numberOfPosts(subreddit.getPosts().size())
+                .build();
     }
 
     private Subreddit mapSubredditDto(SubredditDto subredditDto) {
