db63da63c80dd1f54376b86f3e1969d7e17af6c3
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-14 00:35:03 +0200

    Implement subreddit mapper

---
+++ src/main/java/com/example/shareit/mapper/SubredditMapper.java
@@ -0,0 +1,25 @@
+package com.example.shareit.mapper;
+
+import com.example.shareit.dto.SubredditDto;
+import com.example.shareit.model.Post;
+import com.example.shareit.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
