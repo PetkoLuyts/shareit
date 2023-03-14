2fb8ca128b0b5c8837d23f751643f3dc32b09e50
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-14 08:48:10 +0200

    Implement post dto classes

---
+++ src/main/java/com/example/shareit/dto/PostResponse.java
@@ -0,0 +1,11 @@
+package com.example.shareit.dto;
+
+public class PostResponse {
+
+    private Long id;
+    private String postName;
+    private String url;
+    private String description;
+    private String userName;
+    private String subredditName;
+}
