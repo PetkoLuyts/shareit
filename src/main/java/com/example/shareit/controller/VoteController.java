ac50b7d139366a1a765727e7a6139e208b6b2529
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-17 16:55:58 +0200

    Implement VoteDto class

---
+++ src/main/java/com/example/shareit/controller/VoteController.java
@@ -0,0 +1,14 @@
+package com.example.shareit.controller;
+
+import com.example.shareit.service.VoteService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/votes")
+@AllArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+}
---
+++ src/main/java/com/example/shareit/service/VoteService.java
@@ -0,0 +1,9 @@
+package com.example.shareit.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+}
