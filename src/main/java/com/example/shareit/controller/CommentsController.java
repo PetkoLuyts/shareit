7e43677f6d3416538ea03ccd05f414ff3aae2180
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-17 13:44:10 +0200

    Finish the implementation of post comment endpoint

--- src/main/java/com/example/shareit/controller/CommentsController.java
+++ src/main/java/com/example/shareit/controller/CommentsController.java
@@ -1,7 +1,10 @@
 package com.example.shareit.controller;
 
 import com.example.shareit.dto.CommentsDto;
+import com.example.shareit.service.CommentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/comments")
 @AllArgsConstructor
 public class CommentsController {
+    private final CommentService commentService;
 
     @PostMapping
-    public void createComment(@RequestBody CommentsDto commentsDto) {
+    public ResponseEntity<Object> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
 
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
